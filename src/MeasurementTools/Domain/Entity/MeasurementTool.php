2dbac0fb4da85bc3ae9d05d296e433b280026e5f
Sergey Fadeev <fadeevsn92@gmail.com>
2022-07-22 19:28:08 +0400

    Install doctrine-extension

--- src/MeasurementTools/Domain/Entity/MeasurementTool.php
+++ src/MeasurementTools/Domain/Entity/MeasurementTool.php
@@ -1,10 +1,21 @@
 <?php
 declare(strict_types=1);
 
-
 namespace App\MeasurementTools\Domain\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'measurement_tools')]
 class MeasurementTool
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
 
+    #[ORM\Column(length: 255)]
+    private string $name;
+
+    
 }
