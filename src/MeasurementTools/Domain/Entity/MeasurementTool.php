8d374c396668c85d5787ca4d9a4d9ec82ba5f4e5
Sergey Fadeev <fadeevsn92@gmail.com>
2022-07-22 19:41:02 +0400

    Add MeasurementTool (timestamp and softdelete)

--- src/MeasurementTools/Domain/Entity/MeasurementTool.php
+++ src/MeasurementTools/Domain/Entity/MeasurementTool.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 namespace App\MeasurementTools\Domain\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\SoftDeleteable;
+use Gedmo\Mapping\Annotation\Timestampable;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'measurement_tools')]
+#[SoftDeleteable]
 class MeasurementTool
 {
+    use SoftDeleteableEntity;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -17,5 +23,10 @@ class MeasurementTool
     #[ORM\Column(length: 255)]
     private string $name;
 
-    
+    #[Timestampable(on: 'create')]
+    private \DateTime $createdAt;
+
+    #[Timestampable(on: 'update')]
+    private \DateTime $updatedAt;
+
 }
